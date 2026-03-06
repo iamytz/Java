898491f9aa3d4f2d17cb099b5fc6cfa1ca51fff7
Gabriel do Carmo Pereira <gabrielcarmope@gmail.com>
2026-03-05 22:15:41 -0300

    Aula02 - POO em JAVA

---
+++ cursoEmVideo/pooJava/Caneta.java
@@ -0,0 +1,27 @@
+package cursoEmVideo.pooJava;
+
+public class Caneta{
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Caneta de cor " + this.cor );
+    }
+
+
+    void rabiscar() {
+
+    }
+
+    void tampar () {
+
+    }
+
+    void destampar() {
+
+    }
+
+}
